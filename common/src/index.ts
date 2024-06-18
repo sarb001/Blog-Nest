b87c5d57fe32e66e8bb0ed96832ed3c0e478257b
SARB <mrsinghbusiness05@gmail.com>
2024-06-18 06:42:28 +0530

    npm package published

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,11 +1,11 @@
 import zod from 'zod';
 
+// used for backend
 export const UserSignup  = zod.object({
      name : zod.string(),
      email : zod.string().email(),
      password: zod.string().min(5)
 })
-
 
 export const UserValidation = zod.object({
      email : zod.string().email(),
@@ -16,3 +16,10 @@ export const BlogValidation = zod.object({
      title  : zod.string() ,
      description : zod.string(),
 })
+
+
+// used for frontend for ease 
+
+export type SignupInput =  zod.infer<typeof UserSignup>
+export type SignInValidation =  zod.infer<typeof UserValidation>
+export type BlogVal =  zod.infer<typeof BlogValidation>
