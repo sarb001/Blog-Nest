d97b6b2da8d7d824362648814701207bee5129e4
SARB <mrsinghbusiness05@gmail.com>
2024-06-20 07:28:31 +0530

    fetching Date -PublishedDate

--- frontend/src/components/SingleBlog.tsx
+++ frontend/src/components/SingleBlog.tsx
@@ -2,9 +2,18 @@
 import axios from 'axios';
 import { useParams } from 'react-router-dom'
 import { BACKEND_URL } from '../config';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const SingleBlog = () => {
+
+     interface SingleBlog  {
+        id:number,
+        title : string,
+        description : string,
+        publishedDate : string,
+      }
+      
+    const [singleblog,setsingleblog] = useState<SingleBlog | null>(null);
 
     const { id } = useParams();
     console.log('id is =',id);
@@ -16,15 +25,37 @@ const SingleBlog = () => {
                     'Authorization' : localStorage.getItem('token')
                 }
             });
-            console.log('res =',res);
+            console.log('res =',res.data.blog);
+            setsingleblog(res?.data?.blog);
         }
         FetchsingleBlog();
-    },[])
-
+    },[id]);
 
   return (
-    <div>SingleBlog - {id} </div>
+    <div style = {{padding:'4% 5%'}}>
+     <div>
+            <div key = {id}>
+             <h4> 
+                {singleblog && (
+                    <>
+                        <div key = {singleblog?.id}>
+                        <h3>  {singleblog?.title} </h3>    
+                        <h3> {singleblog?.description} </h3>    
+                        <h3> Posted on  {formatDate(singleblog?.publishedDate)} </h3>    
+                    </div>
+                    </>
+                )}
+            </h4>
+            </div>
+     </div>
+    </div>
   )
 }
 
 export default SingleBlog
+
+
+function  formatDate(date : string){
+    const maindate = new Date(date);
+    return maindate.toDateString();
+}
